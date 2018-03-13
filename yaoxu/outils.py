0a4b44b0b39d0a2ccd4b49f4aba33dd82ee0535b
Redcynthia <redcynthia9022@gmail.com>
2018-03-13 23:46:04 +0100

    new code

--- yaoxu/outils.py
+++ yaoxu/outils.py
@@ -15,6 +15,10 @@ notes_dic = {'C':0, 'C#':1, 'D':2, 'D#':3, \
                 'E':4, 'F':5, 'F#':6, 'G':7, \
                 'G#':8, 'A':9, 'A#':10, 'B':11}
 
+pitchs_dic = {'left4':'C', 'left3':'D', 'left2':'E', 'left1':'F', 'left0':'F#', \
+                'right0':'G', 'right1':'G#', 'right2':'A', 'right3':'A#', 'right4':'B'}
+
+
 def create_note(midiout, pitch, sleep_time, velocity):
     note = re.findall('([A-Z|#]+)[0-9]+', pitch)[0]
     octave = int(re.findall('[A-Z|#]+([0-9]+)', pitch)[0])
@@ -25,3 +29,32 @@ def create_note(midiout, pitch, sleep_time, velocity):
     time.sleep(sleep_time)
     midiout.send_message(note_off)
 
+
+def on_play(midiout, frames, hand, finger_num, octave=5):
+
+    frame = frames[0]
+    previous_frame = frames[1]
+    back_frame = frames[2]
+
+    if hand == 'left':
+        index_1 = frame.hands.leftmost.fingers[finger_num].bone(0).center
+        index_2 = previous_frame.hands.leftmost.fingers[finger_num].bone(0).center
+        index_3 = back_frame.hands.leftmost.fingers[finger_num].bone(0).center
+
+    if hand == 'right':
+        index_1 = frame.hands.rightmost.fingers[finger_num].bone(0).center
+        index_2 = previous_frame.hands.rightmost.fingers[finger_num].bone(0).center
+        index_3 = back_frame.hands.rightmost.fingers[finger_num].bone(0).center
+
+    distance_to_palm_1 = frame.hands.leftmost.palm_position.distance_to(index_1)
+    distance_to_palm_2 = frame.hands.leftmost.palm_position.distance_to(index_2)
+    delta_t = previous_frame.timestamp - back_frame.timestamp
+
+    velo_ = (index_2.y - index_3.y)*1e6/delta_t
+    velo_abs = abs(velo_)*127/500
+    #and velo_abs > 20
+    if (index_2.y-index_1.y)*(index_3.y-index_2.y)<0  and distance_to_palm_1 - distance_to_palm_2 > 1 and index_2.y < 100:   
+        print(finger_num, velo_abs, index_2.y)
+        print distance_to_palm_1, distance_to_palm_2
+        pitch = pitchs_dic[hand + str(finger_num)]+str(octave)
+        create_note(midiout, pitch, 0.1, 50)
--- yaoxu/palm_index_xu_0309.py
+++ yaoxu/palm_index_xu_0309.py
@@ -10,6 +10,7 @@ import numpy as np
 import mido
 import time
 import rtmidi
+from outils import *
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 # Windows and Linux
 #arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
@@ -62,12 +63,18 @@ class SampleListener(Leap.Listener):
         # Get the most recent frame and report some basic information
         frame = controller.frame()
         previous_frame = controller.frame(1)
-        last_frame = controller.frame(10)
-        back_frame = controller.frame(20)
+        back_frame = controller.frame(2)
+        frames = [frame, previous_frame, back_frame]
 
         #print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d, gestures: %d" % (
         #      frame.id, frame.timestamp, len(frame.hands), len(frame.fingers), len(frame.tools), len(frame.gestures()))
 
+
+        for i in range(5):
+            on_play(self.midiout, frames, 'left', i, octave=5)
+            on_play(self.midiout, frames, 'right', i, octave=5)
+        
+        """
         # direction of index finger
         index_y_1 = frame.hands.rightmost.fingers[1].bone(0).center.y
         index_y_2 = last_frame.hands.rightmost.fingers[1].bone(0).center.y
@@ -77,33 +84,44 @@ class SampleListener(Leap.Listener):
         middle_y_2 = last_frame.hands.rightmost.fingers[2].bone(0).center.y
         middle_y_3 = back_frame.hands.rightmost.fingers[2].bone(0).center.y
 
+        delta_t = frame.timestamp - last_frame.timestamp
+
         if (index_y_2-index_y_1)*(index_y_3-index_y_2)<0:
-            print "index finger press action, direction vector: ", index_1, index_2, index_3
+            print "index finger press action, direction vector: ", index_y_1, index_y_2, index_y_3
             # tick of midi is milli second 1e-3second, timestamp of frame is micro second 1e-6second
             #start_ = int((frame.timestamp - self.first_timestamp)/ 1000.0)
             #self.track.append(midi.NoteOnEvent(tick=50, velocity=50, pitch=midi.G_3))
             #self.track.append(midi.NoteOffEvent(tick=150, velocity=50, pitch=midi.G_3))
-            note_on = [0x90, 60, 50] # channel 1, middle C, velocity 112
+            velo_ = (index_y_2 - index_y_1)*1e6/delta_t
+            velo_abs = abs(velo_)*127/2000
+            print(velo_abs)
+            create_note(self.midiout, 'E5', 0.2, velo_abs)
+            
+            note_on = [0x90, 60, velo_abs] # channel 1, middle C, velocity 112
             note_off = [0x80, 60, 0]
             self.midiout.send_message(note_on)
-            time.sleep(0.5)
+            time.sleep(0.1)
             self.midiout.send_message(note_off)
+            
 
 
         if (middle_y_2-middle_y_1)*(middle_y_3-middle_y_2)<0:
-            print "middle finger press action, direction vector: ", middle_1, middle_2, middle_3
+            #print "middle finger press action, direction vector: ", middle_1, middle_2, middle_3
             # tick of midi is milli second 1e-3second, timestamp of frame is micro second 1e-6second
             #start_ = int((frame.timestamp - self.first_timestamp)/ 1000.0)
-            note_on = [0x90, 90, 50] # channel 1, middle C, velocity 112
+            velo_2 = (middle_y_2 - middle_y_1)*1e6/delta_t
+            velo_abs_2 = abs(velo_2)*127/2000
+            note_on = [0x90, 90, velo_abs_2] # channel 1, middle C, velocity 112
             note_off = [0x80, 90, 0]
             self.midiout.send_message(note_on)
-            time.sleep(0.5)
+            time.sleep(0.1)
             self.midiout.send_message(note_off)
         # get the normal vector of the palm
         normal = frame.hands.rightmost.palm_normal
         previous_normal = previous_frame.hands.rightmost.palm_normal
 
-        print normal[0], normal[1], normal[2]
+        """
+        #print normal[0], normal[1], normal[2]
         
 
 
