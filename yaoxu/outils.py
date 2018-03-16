4101f59f0010a0f6ea5aa91838b2dda8162c625e
Redcynthia <redcynthia9022@gmail.com>
2018-03-16 01:11:10 +0100

    new code and pre

--- yaoxu/outils.py
+++ yaoxu/outils.py
@@ -16,7 +16,10 @@ notes_dic = {'C':0, 'C#':1, 'D':2, 'D#':3, \
                 'G#':8, 'A':9, 'A#':10, 'B':11}
 
 pitchs_dic = {'left4':'C', 'left3':'D', 'left2':'E', 'left1':'F', 'left0':'F#', \
-                'right0':'G', 'right1':'G#', 'right2':'A', 'right3':'A#', 'right4':'B'}
+                'right0':'E', 'right1':'F', 'right2':'G', 'right3':'A', 'right4':'B'}
+
+finger_dist_to_palm = [0.5, 1.5, 0.7, 0.9, 1]
+finger_y_flex = [2, 3, 1, 2, 3]
 
 
 def create_note(midiout, pitch, sleep_time, velocity):
@@ -35,26 +38,60 @@ def on_play(midiout, frames, hand, finger_num, octave=5):
     frame = frames[0]
     previous_frame = frames[1]
     back_frame = frames[2]
+    back_5_frame = frames[3]
 
-    if hand == 'left':
-        index_1 = frame.hands.leftmost.fingers[finger_num].bone(0).center
-        index_2 = previous_frame.hands.leftmost.fingers[finger_num].bone(0).center
-        index_3 = back_frame.hands.leftmost.fingers[finger_num].bone(0).center
 
-    if hand == 'right':
-        index_1 = frame.hands.rightmost.fingers[finger_num].bone(0).center
-        index_2 = previous_frame.hands.rightmost.fingers[finger_num].bone(0).center
-        index_3 = back_frame.hands.rightmost.fingers[finger_num].bone(0).center
+    if hand == 'left' and frame.hands.leftmost.is_left:
+        index_1 = frame.hands.leftmost.fingers[finger_num].bone(3).center
+        index_2 = previous_frame.hands.leftmost.fingers[finger_num].bone(3).center
+        index_3 = back_frame.hands.leftmost.fingers[finger_num].bone(3).center
+        index_5 = back_5_frame.hands.leftmost.fingers[finger_num].bone(3).center
+        octave = (frame.hands.leftmost.palm_position.z + 40) // 30
+        distance_to_palm_5 = back_5_frame.hands.leftmost.palm_position.distance_to(index_5)
+        distance_to_palm_2 = previous_frame.hands.leftmost.palm_position.distance_to(index_2)
+    elif hand == 'right' and frame.hands.rightmost.is_right:
+        index_1 = frame.hands.rightmost.fingers[finger_num].bone(3).center
+        index_2 = previous_frame.hands.rightmost.fingers[finger_num].bone(3).center
+        index_3 = back_frame.hands.rightmost.fingers[finger_num].bone(3).center
+        index_5 = back_5_frame.hands.rightmost.fingers[finger_num].bone(3).center
+        octave = (frame.hands.leftmost.palm_position.z + 50) // 30
+        distance_to_palm_5 = back_5_frame.hands.rightmost.palm_position.distance_to(index_5)
+        distance_to_palm_2 = previous_frame.hands.rightmost.palm_position.distance_to(index_2)
+    else:
+        return
 
-    distance_to_palm_1 = frame.hands.leftmost.palm_position.distance_to(index_1)
-    distance_to_palm_2 = frame.hands.leftmost.palm_position.distance_to(index_2)
-    delta_t = previous_frame.timestamp - back_frame.timestamp
+    #if finger_num == 0: print distance_to_palm_5, distance_to_palm_2
+    #and velo_abs > 20 and distance_to_palm_1 - distance_to_palm_2 > 1\
+            #and distance_to_palm_5 - distance_to_palm_2 > finger_dist_to_palm[finger_num] \
+        #and index_5.y - index_2.y > finger_y_flex[finger_num] \
+        #and distance_to_palm_5 - distance_to_palm_2 > finger_dist_to_palm[finger_num] \
+        #and index_1.y < 60 \
 
-    velo_ = (index_2.y - index_3.y)*1e6/delta_t
-    velo_abs = abs(velo_)*127/500
-    #and velo_abs > 20
-    if (index_2.y-index_1.y)*(index_3.y-index_2.y)<0  and distance_to_palm_1 - distance_to_palm_2 > 1 and index_2.y < 100:   
-        print(finger_num, velo_abs, index_2.y)
-        print distance_to_palm_1, distance_to_palm_2
+    if finger_num == 0: distance_to_palm_2, distance_to_palm_5 = distance_to_palm_5, distance_to_palm_2
+    if (index_1.y - index_2.y) * (index_2.y-index_3.y)<0 \
+        and index_5.y - index_2.y > finger_y_flex[finger_num] \
+        and index_1.y < 60:
+        
+
+        
+        delta_t = (previous_frame.timestamp - back_frame.timestamp)/1e6
+        velo_ = (index_2.y - index_3.y)/delta_t
+        velo_abs = abs(velo_)*127/200
+        if velo_abs > 127: velo_abs = 127
+        if octave > 11: octave = 11
+        if octave < 0: octave = 0
+        #print "velo",velo_, velo_abs
+        print hand, finger_num, index_2.y, index_5.y, velo_abs, octave#ndex_5.y, index_2.y#, 
+        
+        #print hand, finger_num, velo_abs, index_2.y
+        #print frame.hands.leftmost.palm_position
+        #print distance_to_palm_1, distance_to_palm_2
         pitch = pitchs_dic[hand + str(finger_num)]+str(octave)
-        create_note(midiout, pitch, 0.1, 50)
+        create_note(midiout, pitch, delta_t, velo_abs)
+
+
+
+
+
+
+
---
+++ yaoxu/palm_index_xu_0315.py
@@ -0,0 +1,162 @@
+################################################################################
+# Copyright (C) 2012-2013 Leap Motion, Inc. All rights reserved.               #
+# Leap Motion proprietary and confidential. Not for distribution.              #
+# Use subject to the terms of the Leap Motion SDK Agreement available at       #
+# https://developer.leapmotion.com/sdk_agreement, or another agreement         #
+# between Leap Motion and you, your company or other organization.             #
+################################################################################
+import os, sys, inspect, thread, time
+import numpy as np
+import mido
+import time
+import rtmidi
+from outils import *
+src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
+# Windows and Linux
+#arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
+# Mac
+arch_dir = os.path.abspath(os.path.join(src_dir, '../../LeapSDK/lib'))
+
+sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
+
+import Leap, sys, thread, time
+import midi
+from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+
+
+class SampleListener(Leap.Listener):
+    finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
+    bone_names = ['Metacarpal', 'Proximal', 'Intermediate', 'Distal']
+    state_names = ['STATE_INVALID', 'STATE_START', 'STATE_UPDATE', 'STATE_END']
+
+
+    def on_init(self, controller):
+        print "Initialized"
+
+    def on_connect(self, controller):
+        print "Connected"
+        self.first_frame = controller.frame()
+        self.first_timestamp = self.first_frame.timestamp
+        self.note_num = 0
+
+        self.midiout = rtmidi.MidiOut()
+        self.available_ports = self.midiout.get_ports()
+        if self.available_ports:
+            self.midiout.open_port(0)
+        else:
+            self.midiout.open_virtual_port("0000")
+
+        # Enable gestures
+        controller.enable_gesture(Leap.Gesture.TYPE_CIRCLE);
+        controller.enable_gesture(Leap.Gesture.TYPE_KEY_TAP);
+        controller.enable_gesture(Leap.Gesture.TYPE_SCREEN_TAP);
+        controller.enable_gesture(Leap.Gesture.TYPE_SWIPE);
+
+    def on_disconnect(self, controller):
+        # Note: not dispatched when running in a debugger.
+        print "Disconnected"
+
+    def on_exit(self, controller):
+        print "Exited"
+
+    def on_frame(self, controller):
+        # Get the most recent frame and report some basic information
+        frame = controller.frame()
+        previous_frame = controller.frame(1)
+        back_frame = controller.frame(2)
+        back_5_frame = controller.frame(5)
+        frames = [frame, previous_frame, back_frame, back_5_frame]
+
+        #print frame.id, frame.timestamp, frame.hands.leftmost.fingers[3].bone(3).center
+        
+        for i in range(5):
+            on_play(self.midiout, frames, 'left', i, octave=5)
+            on_play(self.midiout, frames, 'right', i, octave=5)
+        
+        """
+        # direction of index finger
+        index_y_1 = frame.hands.rightmost.fingers[1].bone(0).center.y
+        index_y_2 = last_frame.hands.rightmost.fingers[1].bone(0).center.y
+        index_y_3 = back_frame.hands.rightmost.fingers[1].bone(0).center.y
+
+        middle_y_1 = frame.hands.rightmost.fingers[2].bone(0).center.y
+        middle_y_2 = last_frame.hands.rightmost.fingers[2].bone(0).center.y
+        middle_y_3 = back_frame.hands.rightmost.fingers[2].bone(0).center.y
+
+        delta_t = frame.timestamp - last_frame.timestamp
+
+        if (index_y_2-index_y_1)*(index_y_3-index_y_2)<0:
+            print "index finger press action, direction vector: ", index_y_1, index_y_2, index_y_3
+            # tick of midi is milli second 1e-3second, timestamp of frame is micro second 1e-6second
+            #start_ = int((frame.timestamp - self.first_timestamp)/ 1000.0)
+            #self.track.append(midi.NoteOnEvent(tick=50, velocity=50, pitch=midi.G_3))
+            #self.track.append(midi.NoteOffEvent(tick=150, velocity=50, pitch=midi.G_3))
+            velo_ = (index_y_2 - index_y_1)*1e6/delta_t
+            velo_abs = abs(velo_)*127/2000
+            print(velo_abs)
+            create_note(self.midiout, 'E5', 0.2, velo_abs)
+            
+            note_on = [0x90, 60, velo_abs] # channel 1, middle C, velocity 112
+            note_off = [0x80, 60, 0]
+            self.midiout.send_message(note_on)
+            time.sleep(0.1)
+            self.midiout.send_message(note_off)
+            
+
+
+        if (middle_y_2-middle_y_1)*(middle_y_3-middle_y_2)<0:
+            #print "middle finger press action, direction vector: ", middle_1, middle_2, middle_3
+            # tick of midi is milli second 1e-3second, timestamp of frame is micro second 1e-6second
+            #start_ = int((frame.timestamp - self.first_timestamp)/ 1000.0)
+            velo_2 = (middle_y_2 - middle_y_1)*1e6/delta_t
+            velo_abs_2 = abs(velo_2)*127/2000
+            note_on = [0x90, 90, velo_abs_2] # channel 1, middle C, velocity 112
+            note_off = [0x80, 90, 0]
+            self.midiout.send_message(note_on)
+            time.sleep(0.1)
+            self.midiout.send_message(note_off)
+        # get the normal vector of the palm
+        normal = frame.hands.rightmost.palm_normal
+        previous_normal = previous_frame.hands.rightmost.palm_normal
+
+        """
+        #print normal[0], normal[1], normal[2]
+        
+
+
+    def state_string(self, state):
+        if state == Leap.Gesture.STATE_START:
+            return "STATE_START"
+
+        if state == Leap.Gesture.STATE_UPDATE:
+            return "STATE_UPDATE"
+
+        if state == Leap.Gesture.STATE_STOP:
+            return "STATE_STOP"
+
+        if state == Leap.Gesture.STATE_INVALID:
+            return "STATE_INVALID"
+
+def main():
+    # Create a sample listener and controller
+    listener = SampleListener()
+    controller = Leap.Controller()
+
+    # Have the sample listener receive events from the controller
+    controller.add_listener(listener)
+
+    # Keep this process running until Enter is pressed
+
+    print "Press Enter to quit..."
+    try:
+        sys.stdin.readline()
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        # Remove the sample listener when done
+        controller.remove_listener(listener)
+
+
+if __name__ == "__main__":
+    main()
